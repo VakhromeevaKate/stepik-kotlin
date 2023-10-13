3b1373e10ba84b6eecf74778b98b001c3740688b
evakhromeeva <evakhromeeva@ozon.ru>
2023-10-13 09:29:11 +0300

    commit

--- _2.4/Collection.kt
+++ _2.4/Collection.kt
@@ -2,7 +2,7 @@ package _2.`4`
 
 fun getCubeList(n:Int): List<Int>{
     // enter your function code here
-    val list: List<Int> = List<Int>(n, {i -> i*i})
+    val list: List<Int> = List<Int>(n, {i -> i*i*i})
     return list
 }
 
